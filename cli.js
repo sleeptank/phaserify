079038378ddd2f49a8abef5eb7b0e5871dad0442
sleeptank <sleeptankxyz@gmail.com>
2017-03-27 00:51:43 -0500

    fix

--- cli.js
+++ cli.js
@@ -13,14 +13,14 @@ if (process.argv[2]) {
 
 const bundle_path = path.join(proj_path, dest, 'phaser-bundle.js')
 
-const check_for_updates = spawn('npm', ['install'], {cwd: base_path, stdio: 'inherit'})
+const check_for_updates = spawn('npm', ['install','-D','phaser-ce','browserify'], {cwd: proj_path, stdio: 'inherit'})
 console.log('Phaserify: Checking for updates...')
 check_for_updates.on('close', (code) => {
   if (code !== 0) {
     throw `Phaserify: update error -> code: ${code}`
   } else {
     console.log('Phaserify: Building Phaser bundle...')
-    const bundle_phaser = spawn('browserify', ['index.js', '-o', bundle_path], {cwd: base_path, stdio: 'inherit'})
+    const bundle_phaser = spawn('browserify', [path.join(base_path, 'index.js'), '-o', bundle_path], {cwd: proj_path, stdio: 'inherit'})
     bundle_phaser.on('close', (code) => {
       if (code !== 0) {
         throw `Phaserify: bundle error -> code: ${code}`
